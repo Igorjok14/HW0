282708e4dfd844a3a7ec95b469fb199b4402d3c8
igor <igorjok1432@gmail.com>
2024-09-27 12:03:31 +0300

    HW0

---
+++ fgh.py
@@ -0,0 +1,15 @@
+from select import select  # "1st program"
+print(9 ** 0.5 * 5)
+ # "2nd program"
+print(9.99 > 9.98) # убедились, что выражение верно
+print(1000 < 1000.1)
+print(9.99 > 9.98 and 1000 < 1000.1)
+ # "3rd program"
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(6 == 8)
+ # "4th program"
+print('123.456')
+print(123.456 * 10)
+int(1234.56)
+print(int(1234.56))
